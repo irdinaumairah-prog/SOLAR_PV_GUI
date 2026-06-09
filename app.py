e882c1b8228cc0ac3bf45ba210218459a949d325
irdinaumairah-prog <sitiirdinaumairah@gmail.com>
2026-06-10 00:12:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ st.markdown("</div>", unsafe_allow_html=True)
 st.markdown("""
 <div class="main-title">
     <h1>Deep Learning-Based Forecasting of Solar PV Output</h1>
-    <p class="subtitle">for Smart Energy Management</p>
+    <p class="subtitle">for Industrial Manufacturing Company</p>
     <p class="prepared"><b>Prepared by:</b> Siti Irdina Umairah</p>
     <p class="prepared"><b>Universiti Tun Hussein Onn Malaysia</b></p>
 </div>
