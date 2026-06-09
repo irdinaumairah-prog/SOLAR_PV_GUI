d49e3aeb4e6b4cce715016655fda1b867582ef9c
irdinaumairah-prog <sitiirdinaumairah@gmail.com>
2026-06-10 00:04:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ st.markdown("""
 # HEADER WITH CENTER LOGO
 # ============================================================
 
-logo_file = "uthm_logo.jpg.new"
+logo_file = "uthm.logo.jpg.new"
 
 st.markdown("""
 <div style="
